ba6a39a59da10696073770fa9ca103f429d422fc
Quốc Quân <116168750+qquan2108@users.noreply.github.com>
2025-07-02 11:53:52 +0700

    Save Google auth users to MongoDB

---
+++ utils/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+// We only use Firebase for authentication. Firestore is removed
+// because user data will be persisted to our MongoDB backend.
+
+const firebaseConfig = {
+  apiKey: 'YOUR_FIREBASE_API_KEY',
+  authDomain: 'your-project.firebaseapp.com',
+  projectId: 'your-project-id',
+  storageBucket: 'your-project.appspot.com',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export default app;
