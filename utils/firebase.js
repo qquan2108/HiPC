2e70c229a7b0a3d85541e0ddcd461fb9f64280e3
QuannxT <quocquan381@gmail.com>
2025-07-02 19:52:40 +0700

    aaaaaaaaaa

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,15 +1,20 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'YOUR_FIREBASE_API_KEY',
-  authDomain: 'your-project.firebaseapp.com',
-  projectId: 'your-project-id',
-  storageBucket: 'your-project.appspot.com',
-  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: "AIzaSyB6S5pYaW3ARIAKIof3HdTgFFLjUsp_X6I",
+  authDomain: "banlaptop-b34ac.firebaseapp.com",
+  databaseURL: "https://banlaptop-b34ac-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "banlaptop-b34ac",
+  storageBucket: "banlaptop-b34ac.firebasestorage.app",
+  messagingSenderId: "625212493169",
+  appId: "1:625212493169:web:73d9d7ee0e3b47d15dfad0",
+  measurementId: "G-2J8T6MF5DG"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export default app;
