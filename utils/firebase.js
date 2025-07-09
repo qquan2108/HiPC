d7f814ecbc22ed1e3ebb39d23181f0a03069c2e5
Quốc Quân <116168750+qquan2108@users.noreply.github.com>
2025-07-09 12:05:08 +0700

    fix google login auth and add error handling

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,20 +1,21 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB6S5pYaW3ARIAKIof3HdTgFFLjUsp_X6I",
-  authDomain: "banlaptop-b34ac.firebaseapp.com",
-  databaseURL: "https://banlaptop-b34ac-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "banlaptop-b34ac",
-  storageBucket: "banlaptop-b34ac.firebasestorage.app",
-  messagingSenderId: "625212493169",
-  appId: "1:625212493169:web:73d9d7ee0e3b47d15dfad0",
-  measurementId: "G-2J8T6MF5DG"
+  apiKey: 'AIzaSyB6S5pYaW3ARIAKIof3HdTgFFLjUsp_X6I',
+  authDomain: 'banlaptop-b34ac.firebaseapp.com',
+  databaseURL: 'https://banlaptop-b34ac-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'banlaptop-b34ac',
+  storageBucket: 'banlaptop-b34ac.firebasestorage.app',
+  messagingSenderId: '625212493169',
+  appId: '1:625212493169:web:73d9d7ee0e3b47d15dfad0',
+  measurementId: 'G-2J8T6MF5DG',
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// Initialize Firebaseconst app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+
+export default app;
