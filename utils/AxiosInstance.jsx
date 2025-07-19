a93393b26ba13ee8fc822652c8d6a561a0698a6e
QuannxT <quocquan381@gmail.com>
2025-07-19 07:58:02 +0700

    fix sth

--- utils/AxiosInstance.jsx
+++ utils/AxiosInstance.jsx
@@ -5,8 +5,8 @@ import { Platform } from 'react-native';
 
 const baseURL =
   Platform.OS === 'ios' || Platform.OS === 'android'
-    ? 'https://7bed11cb3f55.ngrok-free.app' // IP LAN cho thiết bị thật
-    : 'https://7bed11cb3f55.ngrok-free.app';      // Cho web hoặc máy tính
+    ? 'https://hipc-sever-vz03.onrender.com' // IP LAN cho thiết bị thật
+    : 'https://hipc-sever-vz03.onrender.com';      // Cho web hoặc máy tính
 
 // Tạo instance
 const axiosInstance = axios.create({
